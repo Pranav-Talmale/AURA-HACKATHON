80b084799c7df4ff535820942e2910ba68866c85
Aaryan-1304 <aaryanbutala13@gmail.com>
2023-12-14 00:26:28 +0530

    HomePage: Modify Style

--- src/Components/HomeContent.jsx
+++ src/Components/HomeContent.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React, {useState} from 'react';
 import '../Css/home.css';
-import backgroundhome from '../assets/3Dmanbg.png'
-import backgroundhom from '../assets/hompageaura.png'
+import backgroundhome from '../assets/bghome.png'
+// import backgroundhom from '../assets/hompageaura.png'
 import homepagelogo from '../assets/homelogo.png'
 import { useNavigate } from 'react-router';
 export const HomeContent = () => {
@@ -12,15 +12,13 @@ export const HomeContent = () => {
     }
     return (
         <div className="home-container">
-            <div className="image-container">
+            {/* <div className="image-container">
                 <img src={backgroundhom} alt="Image 1" className="homepage-image" />
-            </div>
+            </div> */}
             <div className="text-container">
             <img src={homepagelogo} height={100} width={350} className='productname'/>
                 <h2 className="large-text">
-                Elevate your wealth with our comprehensive financial solutions, 
-                designed to simplify the complexities of managing your finances.
-                <br/>
+                Elevate your wealth with our comprehensive financial solutions, designed to simplify the complexities of managing your finances.
                 Experience the ease of tracking your financial journey today for a prosperous tomorrow.
                 </h2>
                 <button className="action-button" onClick={() => Gotonetworthform()}>Setup your Finances</button>
