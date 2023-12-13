f2fe7ec178afc5d7aafeb635bbe1f7a38f3fa5b2
Pranav-Talmale <pranavtalmale@gmail.com>
2023-12-13 20:08:45 +0530

    Auth: Add bacckend and barebones frontend for login and signup

---
+++ src/Components/SignUpComp.jsx
@@ -0,0 +1,76 @@
+/* eslint-disable no-unused-vars */
+
+import React, {useState, useEffect} from 'react';
+import '../css/signup.css';
+import { SignUp} from '../api/Authapi';
+import { onAuthStateChanged } from 'firebase/auth';
+import {toast} from 'react-toastify';
+import { useNavigate } from "react-router-dom";
+import { auth } from '../firebaseConfig';
+export default function Signup() {
+const [credentials, setCredentials] = useState({});
+let navigate = useNavigate();
+// SIGNUP / REGISTER
+const register = async() => {
+    try {
+        let userdata = await SignUp(credentials.email, credentials.password);
+        toast.success("signing up");
+        localStorage.setItem('userID', userdata.userID);
+        localStorage.setItem('userEmail', userdata.email);
+        navigate('/home');
+    } catch (error) {
+        toast.error(error.message);
+    }
+}
+
+useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+        if(user){
+            navigate('/home');
+        }
+        
+    })
+}, []);
+// REGISTER FORM
+    return (
+        <div className='signupdiv'>
+            <h1 className='heading'>Signup</h1>
+
+            <div className="signupform">
+
+                <label>UserEmail</label>
+
+                <input 
+                type="email" 
+                name="email" 
+                className='signupemail' 
+                placeholder='enter your email id'
+                onChange={(event) => setCredentials({...credentials, email: event.target.value})}
+                />
+
+                <label>Password</label>
+
+                <input 
+                type="password" 
+                name="password" 
+                className='signuppassword' 
+                placeholder='enter your password'
+                onChange={(event) => setCredentials({...credentials, password: event.target.value})}
+                />
+
+
+                <input 
+                type="submit" 
+                name="submit" 
+                className='signupsubmit' 
+                onClick={register}
+                />
+               
+               <p>Already have an account? <a href='/login' className='loginlink'>Login</a></p>
+
+
+            </div>
+
+        </div>
+    )
+}
---
+++ src/Pages/SignUpPage.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import SignUp from "../Components/SignUpComp";
+export default function SignUpPage(){
+    // rendering whole signup page
+    return (
+        <SignUp/>
+    )
+}
